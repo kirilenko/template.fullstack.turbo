677e64685182438c1b0f4ced2d200be5b9d3f212
k8 <n@kirilenko.com>
2026-06-09 11:27:25 +0400

    feat(web-control): add react-render-log, enable via PUBLIC_RENDER_LOG

--- apps/web-control/src/config/env.ts
+++ apps/web-control/src/config/env.ts
@@ -2,6 +2,7 @@ import { parseEnv } from '@packages/lib/env'
 
 const env = parseEnv({
   PUBLIC_API_URL: { required: true, type: 'url' },
+  PUBLIC_RENDER_LOG: { type: 'boolean' },
 })
 
 export { env }
--- apps/web-control/src/main.tsx
+++ apps/web-control/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RenderLogProvider } from 'react-render-log'
 
 import { App } from '@/app'
+import { env } from '@/config'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RenderLogProvider debugEnabled={env.PUBLIC_RENDER_LOG as boolean} isStrictMode={true}>
+      <App />
+    </RenderLogProvider>
   </StrictMode>,
 )
